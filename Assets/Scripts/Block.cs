6a56bfd41efb2ea466e3560f98997c31d469a017
Paweł Lewarski <pawel.lewarski@akmf.pl>
2019-10-28 14:15:54 +0100

    lekcja 71

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+        // cahced reference
+    Level level;
+ 
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+      
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
+        
     }
 }
